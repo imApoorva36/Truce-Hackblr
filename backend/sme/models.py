62c2094082e9c4097384691c875f77b15656eb66
05kashyap <aryankashyapnaveen@gmail.com>
2024-05-11 18:24:26 +0530

    added models

--- backend/sme/models.py
+++ backend/sme/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.contrib.auth import get_user_model
-
-User = get_user_model()
-from django.db import models
 from django.contrib.auth.models import User
 
+User = get_user_model()
 class SME(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='sme_profile')
     name = models.CharField(max_length=255)
@@ -15,6 +13,7 @@ class LoanApplication(models.Model):
     sme = models.ForeignKey(SME, on_delete=models.CASCADE, related_name='loan_applications')
     amount = models.DecimalField(max_digits=15, decimal_places=2)
     address = models.CharField(max_length=30)
+    #nature of businessx
     #purpose = models.TextField()
     #business_plan = models.FileField(upload_to='') # set a path for plans?
     status = models.CharField(max_length=20, choices=[
@@ -25,7 +24,7 @@ class LoanApplication(models.Model):
                                                     ('rejected', 'Rejected')
                                                 ], default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    #updated_at = models.DateTimeField(auto_now=True)
 
 # class BusinessPlanEvaluation(models.Model):
 #     loan_application = models.OneToOneField(LoanApplication, on_delete=models.CASCADE, related_name='business_plan_evaluation')
