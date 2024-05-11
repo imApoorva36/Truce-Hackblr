a159fa70ef9426b3eef26c6defd1203c03326a90
Hariharan <jhariharan2004@gmail.com>
2024-05-11 19:39:22 +0530

    Added login page

---
+++ frontend/src/app/login/schema.js
@@ -0,0 +1,8 @@
+"use client"
+
+import { z } from "zod"
+
+export const formSchema = z.object({
+  email: z.string().min(1, { message: "Kindly enter your email"}).email(),
+  password: z.string().min(1, { message: "Password is required"})
+})
