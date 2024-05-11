e018fb74bb6c59dd5c2ab7252cac129df1302352
Apoorva Agrawal <apoorva3569@gmail.com>
2024-05-11 23:41:26 +0530

    added serializer

--- backend/sme/serializer.py
+++ backend/sme/serializer.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-# from .models import Team
-from django.contrib.auth import get_user_model
-from rest_framework import status
-# class TeamsSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Team
-#         fields = ('name','member1','member2','member3',)
+from .models import SME, LoanApplication, BusinessPlanEvaluation
+
+class SMESerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SME
+        fields = '__all__'
+
+class LoanApplicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LoanApplication
+        fields = '__all__'
+
+class BusinessPlanEvaluationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BusinessPlanEvaluation
+        fields = '__all__'
