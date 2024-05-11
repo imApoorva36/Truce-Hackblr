59acd09ed7bae24369fa0f8e65689e068a9b8d02
Apoorva Agrawal <apoorva3569@gmail.com>
2024-05-11 15:19:18 +0530

    admin fix

--- backend/sme/admin.py
+++ backend/sme/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from .models import Team
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('id','user', 'name', 'contact', 'calculate_score', 'member1','member2','member3')
-    def calculate_score(self,obj):
-        return obj.calculate_score()
-    calculate_score.short_description = 'User Score'
+    list_display = ('id','user', 'name', 'contact', 'member1','member2','member3')
 
 admin.site.register(Team,TeamAdmin)
